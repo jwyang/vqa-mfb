a1a214ba3b038368ea53c89a2095be37c942a540
jwyang <jw2.yang@gmail.com>
2017-11-07 15:30:16 -0500

    add training using hdf5 data

--- mfb_coatt_glove/config.py
+++ mfb_coatt_glove/config.py
@@ -21,8 +21,9 @@ LSTM_UNIT_NUM = 1024
 JOINT_EMB_SIZE = MFB_FACTOR_NUM*MFB_OUT_DIM
 NUM_IMG_GLIMPSE = 2
 NUM_QUESTION_GLIMPSE = 2
-IMG_FEAT_WIDTH = 14
-IMG_FEAT_SIZE = IMG_FEAT_WIDTH * IMG_FEAT_WIDTH
+IMG_FEAT_HEIGHT = 6
+IMG_FEAT_WIDTH = 6
+IMG_FEAT_SIZE = IMG_FEAT_HEIGHT * IMG_FEAT_WIDTH
 MAX_WORDS_IN_QUESTION = 15
 LSTM_DROPOUT_RATIO = 0.3
 MFB_DROPOUT_RATIO = 0.1
@@ -39,23 +40,39 @@ DATA_PATHS = {
 	'train': {
 		'ques_file': VQA_PREFIX + '/raw/annotations/OpenEnded_mscoco_train2014_questions.json',
 		'ans_file': VQA_PREFIX + '/raw/annotations/mscoco_train2014_annotations.json',
-		'features_prefix': VQA_PREFIX + '/features/resnet_%s_bgrms_large/train2014/COCO_train2014_'%feat
+		'features_prefix': VQA_PREFIX + '/features/resnet_%s_bgrms_large/train2014/COCO_train2014_'%feat,
+                'img_prefix': 'COCO_train2014_'
 	},
 	'val': {
 		'ques_file': VQA_PREFIX + '/raw/annotations/OpenEnded_mscoco_val2014_questions.json',
 		'ans_file': VQA_PREFIX + '/raw/annotations/mscoco_val2014_annotations.json',
-		'features_prefix': VQA_PREFIX + '/features/resnet_%s_bgrms_large/val2014/COCO_val2014_'%feat
+		'features_prefix': VQA_PREFIX + '/features/resnet_%s_bgrms_large/val2014/COCO_val2014_'%feat,
+                'img_prefix': 'COCO_val2014_'
 	},
 	'test-dev': {
 		'ques_file': VQA_PREFIX + '/raw/annotations/OpenEnded_mscoco_test-dev2015_questions.json',
-		'features_prefix': VQA_PREFIX + '/features/resnet_%s_bgrms_large/test2015/COCO_test2015_'%feat
+		'features_prefix': VQA_PREFIX + '/features/resnet_%s_bgrms_large/test2015/COCO_test2015_'%feat,
+                'img_prefix': 'COCO_test2015_'
 	},
 	'test': {
-		'ques_file': VQA_PREFIX + '/raw/annotations/OpenEnded_mscoco_test2015_questions.json',
-		'features_prefix': VQA_PREFIX + '/features/resnet_%s_bgrms_large/test2015/COCO_test2015_'%feat
+		'ques_file': VQA_PREFIX + '/raw/annotations/OpenEnded_mscoco_test2   fdafd015_questions.json',
+		'features_prefix': VQA_PREFIX + '/features/resnet_%s_bgrms_large/test2015/COCO_test2015_'%feat,
+                'img_prefix': 'COCO_test2015_',
 	},
 	'genome': {
 		'genome_file': VQA_PREFIX + '/Questions/OpenEnded_genome_train_questions.json',
 		'features_prefix': VQA_PREFIX + '/Features/genome/feat_resnet-152/resnet_%s_bgrms_large/'%feat
 	}
 }
+
+options = {
+        'dir': {
+            'root': 'data/coco'
+        },
+        'model': {
+            'arch': 'resnet101bottomup_size,600',
+            'ftype': 'bottomup',
+            'fuse_mode': 'att',
+        },              
+}
+
